4c98c35b6b5006c8707f188c8a637180a7ce1934
dndudJang <wyjang@rbh.co.kr>
2022-11-01 16:33:03 +0900

    mock data 반영

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,6 +1,7 @@
 const TodoController = require("../../controllers/todo.controller");
 const TodoModel = require("../../models/todo.model");
-
+const httpMocks = require("node-mocks-http");
+const newTodo = require("../mock-data/new-todo.json")
 
 TodoModel.create = jest.fn()
 
@@ -9,7 +10,16 @@ describe("TodoController.createTodo",()=>{
         expect(typeof TodoController.createTodo).toBe("function")
     })
     it("should call TodoModel.create",()=>{
-        TodoController.createTodo();
-        expect(TodoModel.create).toBeCalled();
+        //node-mocks-http 이용하여 요청, 응답객체 생성
+        let req, res, next;
+        req=httpMocks.createRequest();
+        res=httpMocks.createResponse();
+        next = null;
+
+        //mockData를 req.body에 넣어줌
+        req.body=newTodo;
+
+        TodoController.createTodo(req, res, next);
+        expect(TodoModel.create).toBeCalledWith(newTodo);
     })
 })
