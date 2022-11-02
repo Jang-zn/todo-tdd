08693a44a96031137fb8df64b11ab1980bc54eec
dndudJang <wyjang@rbh.co.kr>
2022-11-02 18:00:50 +0900

    controller async처리했으니 unit에서도 변경해줘야 함

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -3,5 +3,5 @@ const TodoModel = require('../models/todo.model');
 
 exports.createTodo= async (req, res, next)=>{
     const createdModel = await TodoModel.create(req.body);
-    res.status(201).json(createdModel).send();
+    res.status(201).json(createdModel);
 }
--- models/todo.model.js
+++ models/todo.model.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const TodoSchema = new mongoose.Schema({
+    id:{
+        type:Number,
+        required:true
+    },
     title : {
         type:String,
         required:true,
