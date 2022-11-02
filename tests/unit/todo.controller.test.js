d963872c1276d4d77301ed2a4ef5be2dc63a256e
dndudJang <wyjang@rbh.co.kr>
2022-11-02 18:00:50 +0900

    controller async처리했으니 unit에서도 변경해줘야 함

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -30,17 +30,17 @@ describe("TodoController.createTodo",()=>{
     });
 
     //응답처리
-    it("should return 201 response code",()=>{
-        TodoController.createTodo(req,res,next);
+    it("should return 201 response code", async ()=>{
+        await TodoController.createTodo(req,res,next);
         expect(res.statusCode).toBe(201);
         //_isEndCalled의 T/F 검증 toBeTruthy-> 실제로 응답을 send() 또는 return 했는지 확인, F면 통과 못함
         expect(res._isEndCalled()).toBeTruthy();
     });
     //리턴하는 값이 우리가 원하는 모델과 데이터 형태가 일치하는지 확인
-    it("should return json body in response",()=>{
+    it("should return json body in response", async ()=>{
         //비교할 값
         TodoModel.create.mockReturnValue(newTodo);
-        TodoController.createTodo(req,res,next);
+        await TodoController.createTodo(req,res,next);
         //객체를 toBe로 비교하면 값 같아도 주소값이 달라서 안됨
         //toStrictEqual() 이용
         expect(res._getJSONData()).toStrictEqual(newTodo);
