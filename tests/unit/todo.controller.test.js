992ff5903eeddfbc078e8b00454e379c0f029920
Jang-zn <zn2309@naver.com>
2022-11-02 16:15:59 +0900

    res 상태에 대한 test코드

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -17,11 +17,20 @@ beforeEach(()=>{
 describe("TodoController.createTodo",()=>{
     it("should have a createTodo function",()=>{
         expect(typeof TodoController.createTodo).toBe("function")
-    })
+    });
     it("should call TodoModel.create",()=>{
         //mockData를 req.body에 넣어줌
         req.body=newTodo;
         TodoController.createTodo(req, res, next);
         expect(TodoModel.create).toBeCalledWith(newTodo);
-    })
+
+    });
+    //응답처리
+    it("should return 201 response code",()=>{
+        req.body = newTodo;
+        TodoController.createTodo(req,res,next);
+        expect(res.statusCode).toBe(201);
+        //_isEndCalled의 T/F 검증 -> 실제로 응답을 send() 또는 return 했는지 확인
+        expect(res._isEndCalled()).toBeTruthy();
+    });
 })
