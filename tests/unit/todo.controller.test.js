22422f73f2d6620239b6a63b244ad4e16cc10c12
dndudJang <wyjang@rbh.co.kr>
2022-11-02 16:32:46 +0900

    model 값 검증 - toStrictEqual()

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -15,22 +15,34 @@ beforeEach(()=>{
 });
 
 describe("TodoController.createTodo",()=>{
+    beforeEach(()=>{
+        req.body=newTodo;
+    });
+
     it("should have a createTodo function",()=>{
         expect(typeof TodoController.createTodo).toBe("function")
     });
     it("should call TodoModel.create",()=>{
         //mockData를 req.body에 넣어줌
-        req.body=newTodo;
+        //createTodo 이후 TodoModel.create가 데이터인 newTodo를 파라미터로 넣고 호출되는지 확인
         TodoController.createTodo(req, res, next);
         expect(TodoModel.create).toBeCalledWith(newTodo);
-
     });
+
     //응답처리
     it("should return 201 response code",()=>{
-        req.body = newTodo;
         TodoController.createTodo(req,res,next);
         expect(res.statusCode).toBe(201);
-        //_isEndCalled의 T/F 검증 -> 실제로 응답을 send() 또는 return 했는지 확인
+        //_isEndCalled의 T/F 검증 toBeTruthy-> 실제로 응답을 send() 또는 return 했는지 확인, F면 통과 못함
         expect(res._isEndCalled()).toBeTruthy();
     });
+    //리턴하는 값이 우리가 원하는 모델과 데이터 형태가 일치하는지 확인
+    it("should return json body in response",()=>{
+        //비교할 값
+        TodoModel.create.mockReturnValue(newTodo);
+        TodoController.createTodo(req,res,next);
+        //객체를 toBe로 비교하면 값 같아도 주소값이 달라서 안됨
+        //toStrictEqual() 이용
+        expect(res._getJSONData()).toStrictEqual(newTodo);
+    })
 })
