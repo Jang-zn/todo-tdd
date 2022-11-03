80490b1694f6d435d73a9c81f17df449c11adc17
dndudJang <wyjang@rbh.co.kr>
2022-11-03 12:13:51 +0900

    postman test

--- app.js
+++ app.js
@@ -18,9 +18,7 @@ app.get('/',(req, res, next)=>{
     res.send('hello tdd');
 });
 
-app.listen(3000, ()=>{
-    console.log('Server is now running')
-})
+
 
 //통합테스트에서 사용하기 위해 export
 module.exports = app;
